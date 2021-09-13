f1b5b4d5d70a9e2ccfb72d07e5e88dc3ebc3248c
Rohan Bansal <rohan@parsimony.com>
2021-09-13 15:32:42 +0530

    fix: bad cherry-pick changes

--- erpnext/accounts/doctype/chart_of_accounts_importer/chart_of_accounts_importer.py
+++ erpnext/accounts/doctype/chart_of_accounts_importer/chart_of_accounts_importer.py
@@ -172,7 +172,7 @@ def build_forest(data):
 				if not parent_account_list and parent_account:
 					frappe.throw(_("The parent account {0} does not exists in the uploaded template").format(
 						frappe.bold(parent_account)))
-				return [child] + parent_account_list
+				return [child] + (parent_account_list or [])
 
 	charts_map = defaultdict(dict)
 	paths = []
