cc6d62a50a99200549b5a0cc229e6c92d3dac433
Rohan Bansal <rohan@parsimony.com>
2023-04-06 15:03:54 +0530

    ci: use version-14 of the payments app

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -57,7 +57,7 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app payments --branch ${githubbranch%"-hotfix"}
+bench get-app payments --branch version-14
 bench get-app erpnext "${GITHUB_WORKSPACE}"
 
 if [ "$TYPE" == "server" ]; then bench setup requirements --dev; fi
